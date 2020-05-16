cef1914e29bbf692ad65b6861721a2945ca8431c
Viktoriia Popovych <mw3feria@gmail.com>
2020-05-17 01:45:43 +0200

    Add edit mode for products

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -48,6 +48,6 @@ export class ProductService {
     return this.http.delete(`${environment.fbObUrl}/products/${id}.json`)
   }
   update(product:Product) {
-    return this.http.patch(`${environment.fbObUrl}/products/${product.id}.json`,product)
+    return this.http.patch(`${environment.fbObUrl}/products/${product.id}.json`, product)
   }
 }
